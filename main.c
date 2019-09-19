da01a99d1945311c3898efbeaba89394305f7b5d
Caleb Alves <54603766+calvez21@users.noreply.github.com>
2019-09-19 14:57:44 -0400

    Final main.c for the dice game
    Not 100% sure if it works

--- main.c
+++ main.c
@@ -1,25 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "StatsLibrary.h"
+#include <math.h>
+#include <time.h>
+
+#define RAND_MAX = 20
+#define WON 0
+#define LOSE 1
+
+int replay;
+int rollDice(void);
+void playGame(int credits, int replay);
+void startGame(void);
+int calculateCredits(int wager, int credits, int computer_roll, int user_roll);
+
+int rollDice(void)
+{
+    return ((rand() % 6) +1);
+}
 
 int main(void)
 {
-    int c;
-    FILE *f;
-    f=fopen("data.txt", "r");
-    if (f == NULL)
+    int dice = 0;
+    int result;
+    int credits;
+    int wager;
+    time_t t;
+    //int replay;
+    srand(time(&t)); //initializer for the random seed
+    printf("Let's play a dice game!");
+    printf("\n");
+    printf("GAME RULES:\n");
+    printf("\n");
+    printf("> You are playing against the computer (me!). The objective is to roll a higher\n");
+    printf(" integer value than me. I will be rolling a 20-sided die.\n");
+    printf("\n");
+    printf("> You will be asked to place a bet before each roll. This bet must be less than or\n");
+    printf(" equal to the number of credit(s) you have available to you.\n");
+    printf("\n");
+    printf("> If you succeed in rolling higher, then you win! I'll give you credits equal to your \n");
+    printf(" wager.\n");
+    printf("\n");
+    printf("> If I roll higher than you, then I get to keep your credits for myself. Don't worry,\n");
+    printf(" I'll take good care of them until you want to play again.\n");
+    printf("\n");
+    printf("> The game is over when you run out of credits or when you close the console window.\n");
+    printf("\n");
+    printf("Would you like to play? 1=Yes, 0=No\n");
+    scanf("%i", &replay);
+    if (replay==0)
     {
-        printf("Error reading in data.txt");
+        printf("Quitter :(");
+        return;
     }
-    for (c= getc(f); c != EOF; c=getc(f))
+    else if (replay==1)
     {
-        c=c+1;
+        startGame();
     }
-    float nums[c];
-    for (int i=0; i<c; i++)
+
+}
+
+void startGame(void)
+{
+    printf("Please enter the amount of credits you wish to start with...\n");
+    int credits;
+    scanf("%i", &credits);
+    int replay;
+    printf("Your credit balance is: %d\n", credits);
+    playGame(credits, replay);
+    return;
+}
+
+void playGame(int credits, int replay)
+{
+    int wager=0;
+    do
     {
-        fscanf(f,"%d", &nums[i]);
+        printf("Please enter the amount you would like to wager, must be less than %d\n", credits);
+        scanf("%d", &wager);
+        printf("Your bet: %d", wager);
+        if (wager>credits)
+        {
+            wager = 0;
+            printf("You do not have enough credits for that wager, please try again\n");
+        }
+        else if (wager<= credits)
+        {
+            int comp_roll=rollDice();
+            int user_roll=rollDice();
+            printf("I rolled a %d and you rolled a %d\n", comp_roll, user_roll);
+            credits = calculateCredits(credits,wager,comp_roll,user_roll);
+            if (credits>0)
+            {
+                printf("Would you like to play again? 0=No 1=Yes\n");
+                scanf("%d", &replay);
+            }
+            else if (credits<=0)
+            {
+                printf("You lose!!\n");
+                replay=0;
+            }
+        }
     }
-    fclose(f);
-    return 0;
+    while (replay==1);
+    return;
+}
+int calculateCredits(int credits,int wager,int computer_roll,int user_roll)
+{
+    if(computer_roll>user_roll)
+    {
+        printf("You lose! I'll be taking those credits\n");
+        credits = credits-wager;
+        printf("Your new credit balance is: %d\n", credits);
+        return(credits);
+    }
+    else if(user_roll>computer_roll)
+    {
+        printf("You win! Dangit!\n");
+        credits = credits+wager;
+        printf("Your new credit balance is: %d\n", credits);
+        return(credits);
+    }
 }
